2d4113720225ad03b43d2433330b8e620dfacdd2
jorgenherje <jorgen.herje@ceetronsolutions.com>
2023-06-19 11:11:46 +0200

    Adjust vite config to prevent duplicate export

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define:{"process.env":{},"global":"window"},  
-})
+export default defineConfig(({ mode }) => {
+  const define = {
+      "process.env": {},
+  };
+
+  // In order to polyfill "global" for older packages
+  // Only in dev since "@loaders.gl" is already exporting "window" and would cause a duplicate export
+  if (mode === "development") {
+      define["global"] = "window";
+  }
+
+  return {
+      plugins: [react()],
+      define: define,
+  }
+});
